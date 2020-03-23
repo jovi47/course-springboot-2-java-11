3959e2e9552f008173b568a192339432e86a5cb1
Joaozin47 <kiritoemailcontatos@gmail.com>
2020-03-22 23:44:22 -0300

    User entity and resource

---
+++ src/main/java/com/joaozin/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.joaozin.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaozin.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "João Victor", "kiritoemailcontatos@gmail.com", "7999923823", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
