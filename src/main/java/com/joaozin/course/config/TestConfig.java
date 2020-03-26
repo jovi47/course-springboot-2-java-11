b7f8c10a88c8b2551ada6ef265e66761e4b58e2d
Joaozin47 <kiritoemailcontatos@gmail.com>
2020-03-26 14:38:16 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/joaozin/course/config/TestConfig.java
+++ src/main/java/com/joaozin/course/config/TestConfig.java
@@ -36,21 +36,31 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Product p1 = new Product(null, "The Lord table", "The best table you ever seen", 150.3, "");
-		Product p2 = new Product(null, "Unao", "A super car", 34000.2, "");
-		Product p3 = new Product(null, "The white house", "A beautiful house ", 150000.2, "");
-		Category category1 = new Category(null,"Eletronics");
-		Category category2 = new Category(null,"Books");
-		Category category3 = new Category(null,"Computers");
+		
+		Product p1 = new Product(null, "The Lord of the rings", "Lorem ipsum dolor sit amer", 150.3, "");
+		Product p2 = new Product(null, "Smart TV ", "Nulla em purus. Macenas ante.", 2340.0, "");
+		Product p3 = new Product(null, "MacBook Pro", "Nam torto, at mollis  ", 2000.0, "");
+		Product p4 = new Product(null, "PC gamer", "Donec aquas odio ac phoncus cursus.  ", 1220.0, "");
+		Product p5 = new Product(null, "Ralls for Dummies", "Car fringilla convallis sem vel faucibus  ", 100.0, "");
+		Category category1 = new Category(null, "Eletronics");
+		Category category2 = new Category(null, "Books");
+		Category category3 = new Category(null, "Computers");
 		User u = new User(null, "José Aécio", "aecio.diskal@gmail.com", "21999252367", "ppirg22");
 		User u2 = new User(null, "Pitoco", "pitoquinho123@gmail.com", "0000023292", "pitoco123");
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.DELIVIRED,u);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID,u2);
-		Order o3 = new Order(null, Instant.parse("2019-08-27T12:33:22Z"),OrderStatus.SHIPPED,u);
-		productRepository.saveAll(Arrays.asList(p1,p2,p3));
-		categoryRepository.saveAll(Arrays.asList(category1,category2,category3));
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVIRED, u);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-08-27T12:33:22Z"), OrderStatus.SHIPPED, u);
+		productRepository.saveAll(Arrays.asList(p1, p2, p3,p4,p5));
+		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+		p1.getCategories().add(category2);
+		p2.getCategories().add(category1);
+		p2.getCategories().add(category3);
+		p3.getCategories().add(category3);
+		p4.getCategories().add(category3);
+		p5.getCategories().add(category2);
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		userRepository.saveAll(Arrays.asList(u, u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 
 }
